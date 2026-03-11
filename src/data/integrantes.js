95ad2f37c5b6eb9ffe6ac2a18886215d1ceb37a4
Jaira Suarez <jairsuarez9806@gmail.com>
2026-03-11 15:05:08 -0500

    6

--- src/data/integrantes.js
+++ src/data/integrantes.js
@@ -2,32 +2,32 @@ const integrantesEquipo = [
   {
     nombre: 'Bill Mahoney',
     cargo: 'PRODUCT OWNER',
-    foto: './public/photo1.png' 
+    foto: '/photo1.png'
   },
   {
     nombre: 'Saba Cabrera',
     cargo: 'ART DIRECTOR',
-    foto: './public/photo2.png'
+    foto: '/photo2.png'
   },
   {
     nombre: 'Shae Le',
     cargo: 'TECH LEAD',
-    foto: './public/photo3.png'
+    foto: '/photo3.png'
   },
   {
     nombre: 'Skylah Lu',
     cargo: 'UX DESIGNER',
-    foto: './public/photo4.png'
+    foto: '/photo4.png'
   },
   {
     nombre: 'Griff Richards',
     cargo: 'DEVELOPER',
-    foto: './public/photo5.png'
+    foto: '/photo5.png'
   },
   {
     nombre: 'Stan John',
     cargo: 'DEVELOPER',
-    foto: './public/photo6.png'
+    foto: '/photo6.png'
   }
 ];
 
